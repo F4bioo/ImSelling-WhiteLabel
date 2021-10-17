673beaded3cdd3626251cbff8ac080c41f382e40
F4bioo <costa.fbo@gmail.com>
2021-10-16 21:47:07 -0300

    Finish firebase data source

--- app/src/main/java/com/fappslab/imselling/data/FirebaseProductDataSource.kt
+++ app/src/main/java/com/fappslab/imselling/data/FirebaseProductDataSource.kt
@@ -5,8 +5,10 @@ import com.fappslab.imselling.BuildConfig
 import com.fappslab.imselling.domain.model.Product
 import com.fappslab.imselling.utils.COLLECTION_PRODUCTS
 import com.fappslab.imselling.utils.COLLECTION_ROOT
+import com.fappslab.imselling.utils.STORAGE_IMAGES
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import java.util.*
 import kotlin.coroutines.suspendCoroutine
 
 class FirebaseProductDataSource(
@@ -39,10 +41,34 @@ class FirebaseProductDataSource(
     }
 
     override suspend fun uploadProductImage(imageUri: Uri): String {
-        TODO("Not yet implemented")
+        return suspendCoroutine { continuation ->
+            val randomKey = UUID.randomUUID()
+            val childReference = storageReference.child(
+                "$STORAGE_IMAGES/${BuildConfig.FIREBASE_FLAVOR_COLLECTION}/$randomKey"
+            )
+
+            childReference.putFile(imageUri).addOnSuccessListener { taskSnapshot ->
+                taskSnapshot.storage.downloadUrl.addOnSuccessListener { uri ->
+                    val path = uri.toString()
+                    continuation.resumeWith(Result.success(path))
+                }
+            }.addOnFailureListener { exception ->
+                continuation.resumeWith(Result.failure(exception))
+            }
+        }
     }
 
     override suspend fun createProducts(product: Product): Product {
-        TODO("Not yet implemented")
+        return suspendCoroutine { continuation ->
+            documentReference
+                .collection(COLLECTION_PRODUCTS)
+                .document(System.currentTimeMillis().toString())
+                .set(product)
+                .addOnSuccessListener {
+                    continuation.resumeWith(Result.success(product))
+                }.addOnFailureListener { exception ->
+                    continuation.resumeWith(Result.failure(exception))
+                }
+        }
     }
 }

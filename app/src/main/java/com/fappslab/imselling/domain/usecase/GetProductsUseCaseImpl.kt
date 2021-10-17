5d6c5867e1c6d9eafd799c30eb8f9f8ce0584724
F4bioo <costa.fbo@gmail.com>
2021-10-17 19:30:29 -0300

    Implement error handler

--- app/src/main/java/com/fappslab/imselling/domain/usecase/GetProductsUseCaseImpl.kt
+++ app/src/main/java/com/fappslab/imselling/domain/usecase/GetProductsUseCaseImpl.kt
@@ -1,16 +1,26 @@
 package com.fappslab.imselling.domain.usecase
 
 import com.fappslab.imselling.data.ProductRepository
+import com.fappslab.imselling.domain.ErrorHandler
 import com.fappslab.imselling.domain.model.Product
+import com.fappslab.imselling.domain.type.ResultType
 import javax.inject.Inject
 
 class GetProductsUseCaseImpl
 @Inject
 constructor(
-    private val productRepository: ProductRepository
+    private val productRepository: ProductRepository,
+    private val errorHandler: ErrorHandler
 ) : GetProductsUseCase {
 
-    override suspend fun invoke(): List<Product> {
-        return productRepository.getProducts()
+    override suspend fun invoke(): ResultType<List<Product>> {
+        return try {
+            val products = productRepository.getProducts()
+            ResultType.Success(products)
+
+        } catch (throwable: Throwable) {
+            val error = errorHandler.getError(throwable)
+            ResultType.Error(error)
+        }
     }
 }
